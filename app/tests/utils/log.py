1d28834357a052a15709693136df92a5529c6924
Wodoame <bmwodoame@gmail.com>
2025-06-02 11:22:05 +0000

    feat: add a utility function for creating a random exception log

--- app/tests/utils/log.py
+++ app/tests/utils/log.py
@@ -21,3 +21,22 @@ def create_dummy_logs(database: Session, num_logs: int) -> None:
         )
         database.add(log)
     database.commit()
+
+def create_random_log(database: Session) -> ExceptionLog:
+    """
+    Create a random exception log in the database.
+    """
+    log = ExceptionLog(
+        id=uuid.uuid4(),
+        username="deleteuser",
+        error_type="KeyError",
+        error_message="Delete me",
+        stack_trace="Traceback\n...",
+        timestamp=datetime.now(timezone.utc),
+        path="/delete",
+        method="DELETE",
+        client_ip="127.0.0.1"
+    )
+    database.add(log)
+    database.commit()
+    return log
